474820e2496544aad8c7e05609fcc8be402e738e
mafangyuan <fangyuan.ma@panduo.com.cn>
2019-05-27 09:13:59 +0800

    1.0.2

--- pages/register/register.js
+++ pages/register/register.js
@@ -50,7 +50,7 @@ Page({
       url: 'https://cj.panduo.com.cn/api/new_products_infomation_input/checklogin',
       data: {
         us_user_id: e.detail.value,
-        env:'true'
+        env:'false'
       },
       header: {
         'content-type': 'application/json' // 默认值
@@ -129,7 +129,7 @@ Page({
       data: {
         us_user_id: gonghao,
         us_password: that.data.us_password,
-        env:'true'
+        env:'false'
       },
       header: {
         'content-type': 'application/json' // 默认值
